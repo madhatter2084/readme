2346bc472084d280ee774105b5cb0ea42f9c4bcf
madhatter <matthew.rikard@gmail.com>
2020-08-23 10:18:04 -0400

    inquirer questions added

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const generateREADME = require("./generateMarkdown")
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+inquirer.prompt([
+    {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "Describe your project.",
+    },
+    {
+    type: "input",
+    name: "installation",
+    message: "Tell me how to install it.",
+    },
+    {
+    type: "input",
+    name: "usage",
+    message: "How do I use this app?",
+    },
+    {
+    type: "input",
+    name: "license",
+    message: "What licenses are used?",
+    },
+    {
+    type: "input",
+    name: "contributions",
+    message: "Who contributed to the project?",
+    },
+    {
+    type: "input",
+    name: "tests",
+    message: "What tests were used?",
+    },
+    {
+    type: "input",
+    name: "profile",
+    message: "Link your GitHub profile",
+    },
+    {
+    type: "input",
+    name: "title",
+    message: "",
+    },
+]).then(function(data){
+    generateREADME(data);
+})
